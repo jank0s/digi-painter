78c8a5073b2b09d0e87a0847a714c574b34db247
Jan Kos <jan@k0s.si>
2017-04-25 17:55:17 +0200

    Transition to processing and images converted to png

---
+++ app/js/sketch.js
@@ -0,0 +1,13 @@
+var headingImg;
+function preload() {
+  headingImg = loadImage("images/naslov.png");
+}
+
+function setup() {
+	createCanvas(800, 600);
+	image(headingImg, 0, 0);
+}
+
+function draw() {
+
+}
