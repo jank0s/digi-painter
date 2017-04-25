c767d90ba573f97197991d435c2508e8930761a1
Jan Kos <jan@k0s.si>
2017-04-25 18:11:36 +0200

    Sounds added

--- js/sketch.js
+++ js/sketch.js
@@ -1,13 +1,33 @@
 var headingImg;
+var track01Sound;
+var track02Sound;
+var track03Sound;
+var track04Sound;
+
+var isPlaying = false;
+
 function preload() {
-  headingImg = loadImage("images/naslov.png");
+  headingImg = loadImage('assets/naslov.png');
+  track01Sound = loadSound('assets/track01.wav');
+  track02Sound = loadSound('assets/track02.wav');
+  track03Sound = loadSound('assets/track03.wav');
+  track04Sound = loadSound('assets/track04.wav');
 }
 
 function setup() {
-	createCanvas(800, 600);
+	var canvas = createCanvas(800, 600);
+	canvas.parent('sketch-holder');
+
+	track01Sound.playMode('restart');
+
 	image(headingImg, 0, 0);
 }
 
 function draw() {
+
+}
+
+function mousePressed() {
+	track01Sound.play();
 
 }
